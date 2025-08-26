fc053962af7d5e073a29562494674dccefe2ea10
synchrovision <hatano@catpow.info>
2025-08-26 10:15:11 +0900

    feat 👍 add convertLinksToMarkDown function

--- src/main.js
+++ src/main.js
@@ -1,3 +1,31 @@
-function main() {
-  cosole.log('Catpow GAS');
+function convertLinksToMarkDown() {
+	walkActiveCells((cell) => {
+		const richTexts = cell.getRichTextValues();
+		for (const i in richTexts) {
+			for (const j in richTexts[i]) {
+				const url = richTexts[i][j].getLinkUrl();
+				if (!!url) {
+					const r = richTexts[i][j];
+					const text = r.getText();
+					const md = `[${text}](${url})`;
+					console.log(`replace "${text}" to "${md}"`);
+					richTexts[i][j] = r.copy().setText(md).build();
+				}
+			}
+		}
+		cell.setRichTextValues(richTexts);
+	});
+}
+
+function walkActiveCells(callback) {
+	var app = SpreadsheetApp;
+	var spread = app.getActive();
+	var sheet = app.getActiveSheet();
+	for (const range of sheet.getActiveRangeList().getRanges()) {
+		for (let r = 1; r <= range.getNumRows(); r++) {
+			for (let c = 1; c <= range.getNumColumns(); c++) {
+				callback(range.getCell(r, c));
+			}
+		}
+	}
 }
